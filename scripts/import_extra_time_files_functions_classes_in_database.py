c01b572b0eb080726fdf21325cbeba90e16bd465
joaohenggeler <joaohenggeler@gmail.com>
2021-06-03 21:23:15 +0100

    Import EXTRA_TIME tables selectively.

--- scripts/import_extra_time_files_functions_classes_in_database.py
+++ scripts/import_extra_time_files_functions_classes_in_database.py
@@ -3,8 +3,6 @@
 """
 	This script imports the EXTRA_TIME_FILES, *_FUNCTIONS, and *_CLASSES tables into the database. This is done by using the SQL scripts present in
 	the dataset's directory. Note that this process takes a long time to complete.
-
-	@TODO: Replace the test case with the real paths.
 """
 
 import glob
@@ -21,13 +19,17 @@ with Database() as db:
 	project_name_list = [project.database_name for project in Project.get_project_list_from_config()]
 
 	script_pattern = os.path.join(GLOBAL_CONFIG['dataset_path'], '**', r'EXTRA-TIME-*.sql')
+	tables_to_import = [name.upper() for name in GLOBAL_CONFIG['extra_time_tables_to_import']]
 
 	for script_path in glob.iglob(script_pattern, recursive=True):
 
 		directory_name = os.path.basename(os.path.dirname(script_path))
-		if directory_name in project_name_list:
+		script_name = os.path.basename(script_path).upper()
 
-			success, output = db.execute_script('C:\\Work\\GitHub\\estagio-software-vulnerabilities-2020-2021\\scripts\\modules\\test_cases\\test.sql')
+		# Skip other projects like Derby and Tomcat, and only import data for the EXTRA_TIME tables we want.
+		if directory_name in project_name_list and any(name in script_name for name in tables_to_import):
+
+			success, output = db.execute_script(script_path)
 
 			if success:
 				log.info(f'Imported the data from "{script_path}" successfully.')
